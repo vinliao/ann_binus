5d8e24845b1221ac946fa1020dae5500696c97db
Vincent Liao <vincent.liao@protonmail.com>
2018-12-13 15:19:54 +0700

    fix cluster

--- cluster.py
+++ cluster.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import csv
+from matplotlib import pyplot as plt
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
@@ -47,16 +48,76 @@ def preprocessing():
     pca_res = pca.fit_transform(normalize_res)
     
     return pca_res
-    
-def train(train_data):
-    #clustering algo: input 3, cluster = i don't know
-    
-    #5000 epoch
-    return 0
 
-def visualize(train_result):
-    #use matplotlib
-    return 0
+class SOM:
+    def __init__(self, height, width, inp_dimension):
+        # step 1 inisialisasi
+        self.height = height
+        self.width = width
+        self.inp_dimension = inp_dimension
+
+        # step 2 inisialisasi node
+        self.node = [tf.to_float([i,j]) for i in range(height) for j in range(width)]
+        self.input = tf.placeholder(tf.float32,[inp_dimension])
+        self.weight = tf.Variable(tf.random_normal([height*width, inp_dimension]))
+
+        # buat fungsi cari node terdekat, update weight
+        self.best_matching_unit = self.get_bmu(self.input)
+        self.updated_weight = self.get_update_weight(self.best_matching_unit, self.input)
+    
+    def get_bmu(self, input):
+        # hitung distances terpendek pake euclidean
+        expand_input = tf.expand_dims(input, 0)
+        euclidean = tf.square(tf.subtract(expand_input, self.weight))
+        distances = tf.reduce_sum(euclidean,1) # 0 akan menambahkan secara kesamping
+
+        # cari index distance terpendek
+        min_index = tf.argmin(distances, 0) # 1 akan mengkecek ke samping | 0 ke bawah
+        bmu_location = tf.stack([tf.mod(min_index, self.width), tf.div(min_index, self.width)])
+        return tf.to_float(bmu_location)
+
+    def get_update_weight(self, bmu, input):
+        # inisialisasi learning rate & sigma
+        learning_rate = 0.5
+        sigma = tf.to_float(tf.maximum(self.height, self.width)/ 2)
+
+        # hitung perbedaan jarak bmu ke node lain pake euclidean(bmu = best matching unit)
+        expand_bmu = tf.expand_dims(bmu, 0)
+        euclidean = tf.square(tf.subtract(expand_bmu, self.node))
+        distances = tf.reduce_sum(euclidean, 1)
+        ns = tf.exp(tf.negative(tf.div(distances**2, 2*sigma**2)))
+
+        # hitung rate
+        rate = tf.multiply(ns, learning_rate)
+        num_of_node = self.height * self.width
+        rate_value = tf.stack([tf.tile(tf.slice(rate,[i],[1]), [self.inp_dimension]) for i in range(num_of_node)])
+
+        # hitung update weight-nya
+        weight_diff = tf.multiply(rate_value, tf.subtract(tf.stack([input for i in range (num_of_node)]), self.weight))
+
+        updated_weight = tf.add(self.weight, weight_diff)
+        return tf.assign(self.weight, updated_weight)
+    
+    def train(self, dataset, num_of_epoch):
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for i in range (num_of_epoch):
+                for data in dataset:
+                    feed = { self.input : data}
+                    sess.run([self.updated_weight],feed)
+            self.weight_value  = sess.run(self.weight)
+            self.node_value = sess.run(self.node)
+            cluster = [ [] for i in range(self.width)]
+            for i, location in enumerate(self.node_value): # diakan meng-return index-nya
+                cluster[int(location[0])].append(self.weight_value[i])
+            self.cluster = cluster
 
 data = preprocessing()
-print(data[:5])
+
+print(data.shape)
+
+som = SOM(5,5,3)
+som.train(data, 5000)
+
+plt.imshow(som.cluster)
+plt.show()
